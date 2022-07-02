80a55c4a982cb40af685a87128f4b953151e573b
Dan Vukelich <daniel@vukeli.ch>
2022-07-02 14:13:52 -0400

    Fix invalid config.py comment

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 class GardenBotConfig():
-    # Set to True when you're running on a raspberry pi.  False for when developing locally.
+    # Set to False when you're running on a raspberry pi.  True for when developing locally.
     USE_MOCK_PINS = True
