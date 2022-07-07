e88ac7e64d361b431616bf3f711ce28a432f6292
Dan Vukelich <daniel@vukeli.ch>
2022-07-07 00:35:18 -0400

    Fix typo in comment

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class GardenBotConfig():
     TEMPLATES_AUTO_RELOAD = True
     # Set to False when you're running on a raspberry pi.  True for when developing locally.
     USE_MOCK_PINS = True
-    # The hall-effect flowmeter used takes frequency and divides it by this number to derive flow in L/minimum
+    # The hall-effect flowmeter used takes frequency and divides it by this number to derive flow in L/min
     # Read the specs of your flowmeter to derive this value
     FLOW_MAGIC_COEFFICIENT = 21.0
